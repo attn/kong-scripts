cb1635da07ea1f9b392c2d6b2796d72976dada48
M. Maxwell Watson <hi@mmwtsn.com>
2015-06-23 12:05:13 -0400

    Formats script into multi-line commands

--- setup_kong.sh
+++ setup_kong.sh
@@ -27,7 +27,10 @@ mkdir /etc/kong || error "Could not create /etc/kong"
 
 # Pull latest YAML config from GitHub
 info "Pulling down latest Kong config"
-curl --silent https://raw.githubusercontent.com/Mashape/docker-kong/master/config.docker/kong.yml > /etc/kong/kong.yml || error "Could not pull down latest Kong config from GitHub"
+curl --silent \
+  https://raw.githubusercontent.com/Mashape/docker-kong/master/config.docker/kong.yml \
+  > /etc/kong/kong.yml \
+  || error "Could not pull down latest Kong config from GitHub"
 
 # Create directory for local data storage on host
 info "Creating /var/lib/cassandra"
@@ -35,11 +38,26 @@ mkdir /var/lib/cassandra || error "Could not create /var/lib/cassandra"
 
 # Spin up new Cassandra container mounted to the local file system
 info "Spinning up new Cassandra container"
-docker run -d -v /var/lib/cassandra:/var/lib/cassandra -p 9042:9042 --name cassandra mashape/cassandra || error "Could not spin up Cassandra instance"
+docker run -d \
+  -v /var/lib/cassandra/:/var/lib/cassandra \
+  -p 9042:9042 \
+  --name cassandra \
+  mashape/cassandra \
+  || error "Could not spin up Cassandra instance"
 
 # Spin up new Docker container consuming the local config we just made
 info "Spinning up new Kong container"
-docker run -d -v /etc/kong:/etc/kong -p 8000:8000 -p 8001:8001 --link cassandra:cassandra --name kong mashape/kong || error "Could not spin up Kong instance"
+docker run -d \
+  -v /etc/kong/:/etc/kong \
+  -p 8000:8000 \
+  -p 8001:8001 \
+  --link cassandra:cassandra \
+  --name kong \
+  mashape/kong \
+  || error "Could not spin up Kong instance"
+
+# Ensure Kong container is running
+info "Make sure Kong container is running"
 
 # Print success message and exit
 success "Kong is ready to go!"
